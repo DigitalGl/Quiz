d527e5ec77c82ef3e63e35d7f4ae40370e17754f
DigitalGl <daniil424342@gmail.com>
2025-08-31 18:39:42 +0500

    new day

---
+++ index.js
@@ -0,0 +1,117 @@
+
+
+const welcomeScreen = document.getElementById("welcome-screen");
+const questionScreen = document.getElementById("question-screen");
+const resultScreen = document.getElementById("result-screen");
+
+
+const playerForm = document.getElementById("player-form");
+const playerNameInput = document.getElementById("player-name");
+
+const сurrentPlayerDisplay = document.getElementById("current-player");
+const сurrentQuestionDisplay = document.getElementById("current-question");
+const totalQuestionsDisplay = document.getElementById("total-questions");
+const currentScoreDisplay = document.getElementById("current-score");
+
+const questionText = document.getElementById("question-text");
+const optionsContainer = document.getElementById("options-container");
+
+const feedBackContainer = document.getElementById("feedback-container");
+const feedbackText = document.getElementById("feedback-text");
+const nextButton = document.getElementById("next-button");
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
